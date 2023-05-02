a4b3c334b1a1e3fb281c4234c24e226fbabcddf7
Daxa <kakdiyadaxa@gmail.com>
2023-05-02 17:37:22 +0100

    5 added

--- src/main/java/org/example/RegisterPage.java
+++ src/main/java/org/example/RegisterPage.java
@@ -3,12 +3,12 @@ import org.openqa.selenium.By;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 public class RegisterPage extends Utils{
-    private By _firstName = By.id("FirstName");
-    private By _lastName = By.id("LastName");
-    private By _email = By.id("Email");
-    private By _password = By.id("Password");
-    private By _confirmPassword = By.id("ConfirmPassword");
-    private By _clickOnRegisterButton = By.id("register-button");
+    private By _firstName = By.xpath("//input[@id=\"FirstName\"]");
+    private By _lastName = By.xpath("//input[@id=\"LastName\"]");
+    private By _email = By.xpath("//input[@id=\"Email\"]");
+    private By _password = By.xpath("//input[@id=\"Password\"]");
+    private By _confirmPassword = By.xpath("//input[@id=\"ConfirmPassword\"]");
+    private By _clickOnRegisterButton = By.xpath("//button[@name=\"register-button\"]");
     public void enterRegistrationDetails(){
         //type firstname
         typeText(_firstName, "FirstnameTest");
